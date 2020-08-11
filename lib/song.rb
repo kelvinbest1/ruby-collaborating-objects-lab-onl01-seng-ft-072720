a5559abaa36b78f78d8c1a0bc694b44ab528af1a
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-11 18:15:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @@all = song
+    binding.pry
   end
 
   def self.new_by_filename(file)
